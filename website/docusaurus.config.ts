52446e9a34c4ebf727ca3a07b4eec131973226a6
Nikita Patsakula <nikita.patsakula@gmail.com>
2026-08-03 00:59:40 +0300

    [*] website: move docs to svod.vpermilp.online

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: "https://docs.morok.tech",
+  url: "https://svod.vpermilp.online",
   baseUrl: "/",
 
   organizationName: "Patsakula Nikita",
